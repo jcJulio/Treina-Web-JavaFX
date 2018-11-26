74e3b72fadeaf7a099a0a4e3e8218ea159008d33
jcjuliocesar50@gmail.com <jcjuliocesar50@gmail.com>
2018-11-26 16:15:37 -0200

    Commit Inicial

---
+++ tw-agenda-fx/src/br/com/treinaweb/agenda/fx/Main.java
@@ -0,0 +1,29 @@
+package br.com.treinaweb.agenda.fx;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			VBox root = (VBox)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root,600,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Minha Agenda");
+			primaryStage.setResizable(false); //nao permite maximizar a janela
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
